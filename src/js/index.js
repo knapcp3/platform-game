9702a64f936d3bf752b5f4ded3003136e4b7dfe0
Jakub Knapik <jakubknapik3@gmail.com>
2019-02-16 03:25:23 +0100

    Test classes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
-import "@babel/polyfill";
-import levels from "./levels";
+import '@babel/polyfill'
+import { simpleLevelPlan } from './Level/levels'
+import Level from './Level/Level'
+import Player from './Actors/Player'
+import Lava from './Actors/Lava'
+import Vec from './modules/Vec'
 
-document.getElementById("output").innerHTML = `Helloo ${levels}!`;
+let simpleLevel = new Level(simpleLevelPlan)
+console.log(`${simpleLevel.width} by ${simpleLevel.height}`)
 
+let player = Player.create(new Vec(1, 1))
+console.log(player.pos)
+
+let lava = Lava.create(new Vec(1, 1), 'v')
+console.log(lava.reset)
