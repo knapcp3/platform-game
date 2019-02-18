9e1bdc61cbf3d6563a8d4e6374cf28d9c131e0f3
Jakub Knapik <jakubknapik3@gmail.com>
2019-02-18 15:50:20 +0100

    Create functions to run a game

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import '@babel/polyfill'
 import css from './../styles/styles.css'
 
-import { simpleLevelPlan } from './Level/levels'
 import DOMDisplay from './Display/DOMDisplay'
 import Level from './Level/Level'
 import Player from './Actors/Player'
@@ -9,7 +8,11 @@ import Lava from './Actors/Lava'
 import Vec from './modules/Vec'
 import { drawGrid } from './Display/drawing'
 import State from './modules/State'
+import { runGame } from './modules/run'
+import { GAME_LEVELS, simpleLevelPlan } from './Level/game-levels'
 
-let simpleLevel = new Level(simpleLevelPlan)
-let display = new DOMDisplay(document.body, simpleLevel)
-display.syncState(State.start(simpleLevel))
+// let simpleLevel = new Level(simpleLevelPlan)
+// let display = new DOMDisplay(document.body, simpleLevel)
+// display.syncState(State.start(simpleLevel))
+
+runGame(GAME_LEVELS, DOMDisplay)
